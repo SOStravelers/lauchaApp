6c3690e6ae1da22371b78d1b2d28a0e32427c023
JohannSch <jschacosta@gmail.com>
2024-02-25 15:08:29 -0300

    cambio database

--- src/db.js
+++ src/db.js
@@ -1,24 +1,29 @@
 import mongoose from "mongoose";
 import envar from "./config/envar.js";
 
-
 const dbConfig = {
   dev: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_DEV}`,
-  test: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_TEST}`,
-  production: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${envar().DB_PROD}`,
+  test: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${
+    envar().DB_TEST
+  }`,
+  production: `mongodb+srv://${envar().DB_USER}:${envar().DB_PASS}@${
+    envar().DB_PROD
+  }`,
 };
 
-const url = "mongodb+srv://coloro:Pepekika@pollagol.5pmli.mongodb.net/pollagol?retryWrites=true&w=majority"
+const url = `mongodb+srv://${envar().DB_USER}:${
+  envar().DB_PASS
+}@pollagol.5pmli.mongodb.net/pollagol?retryWrites=true&w=majority`;
 const env = process.env.NODE_ENV || "";
-console.log(url)
-mongoose.set('strictQuery', false)
+console.log(url);
+mongoose.set("strictQuery", false);
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true
-  });
-  
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false,
+  // useCreateIndex: true
+});
+
 let db = mongoose.connection;
 
 export default db;
